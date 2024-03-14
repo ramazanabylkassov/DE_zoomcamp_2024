a9286015c6b29c96fd4275bc97172eca745bbc7c
ramazanabylkassov <ramazan.abylkassov@gmail.com>
2024-03-14 07:08:55 +0000

    stream demo 1

---
+++ module_6/other_srcs/demo_stream_1/stream.py
@@ -0,0 +1,15 @@
+import faust
+from taxi_rides import TaxiRide
+from faust import current_event
+
+
+app = faust.App('datatalksclub.stream.v2', broker='kafka://localhost:9092')
+topic = app.topic('datatalkclub.yellow_taxi_ride.json', value_type=TaxiRide)
+
+@app.agent(topic)
+async def start_reading(records):
+    async for record in records:
+        print(record)
+
+if __name__ == '__main__':
+    app.main()
